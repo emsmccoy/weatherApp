b49219b314a9b55f5d164b5c770d5ea8eac9b601
Emma <emmcoy7@gmail.com>
2025-02-26 13:11:48 +0100

    feat: create basic returns in pages and implement router (not working)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import Weather from './Weather'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+import { ThemeProvider, createTheme} from '@mui/material/styles';
+import Layout from './layout/Layout';
+import Home from './pages/Home.jsx';
+import Weather from './pages/Weather.jsx';
+import Map from './pages/Map.jsx';
+import About from './pages/About.jsx';
 
+const theme = createTheme();
 
 export default function App() {
   return (
-    <>
-      <Weather />
-    </>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="weather" element={<Weather />} />
+            <Route path="map" element={<Map />} />
+            <Route path="about" element={<About />} />    
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
